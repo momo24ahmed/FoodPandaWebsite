b76a9736a6ebdde4e5c9351ef896715c1e34ba8b
momo24ahmed <157011145+momo24ahmed@users.noreply.github.com>
2024-10-10 02:11:20 +0500

    Add files via upload

---
+++ welcome/script.js
@@ -0,0 +1,10 @@
+// Get the modal
+var modal = document.getElementById("welcomeModal");
+
+// Open the modal
+modal.style.display = "block";
+
+// Close the modal
+function closeModal() {
+    modal.style.display = "none";
+}
